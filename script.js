4e742839a2c80fbfeeba68facb5b0b528bd28e2a
lee-serz <serzh.li.04.06@yandex.ru>
2023-11-16 22:04:39 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let input = document.querySelector("#input");
+let taskList = document.querySelector(".tasks");
+
+function addTask() {
+  event.preventDefault();
+  if (input.value === "") {
+    alert("Заполните поле ввода!");
+  } else {
+    let task = document.createElement("li");
+    task.innerHTML = input.value;
+    taskList.appendChild(task);
+    let span = document.createElement("span");
+    span.innerHTML = "&#215";
+    task.appendChild(span);
+  }
+  input.value = "";
+  saveData();
+}
+
+taskList.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("check");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", taskList.innerHTML);
+}
+
+function showData() {
+  taskList.innerHTML = localStorage.getItem("data");
+}
+
+showData();
